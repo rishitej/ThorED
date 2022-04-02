e8ade9dc72e556ab589491ee3dbbe6b4d08f3477
rishi <rishitej9@gmail.com>
2022-04-02 23:54:39 +0530

    added authentication

--- Android/app/src/main/java/com/example/thored/MainActivity.java
+++ Android/app/src/main/java/com/example/thored/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.thored;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.thored.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signupTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- Android/app/src/main/java/com/example/thored/RegisterActivity.java
+++ Android/app/src/main/java/com/example/thored/RegisterActivity.java
@@ -3,12 +3,75 @@ package com.example.thored;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
+import android.view.View;
+
+import com.example.thored.databinding.ActivityMainBinding;
+import com.example.thored.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = binding.emailREt.getText().toString().trim();
+                String username = binding.usernameREt.getText().toString().trim();
+                String password = binding.passwordREt.getText().toString().trim();
+                String confirmpassword = binding.confirmpasswordREt.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    binding.emailREt.setError("Enter Email!");
+                    binding.emailREt.requestFocus();
+                    return;
+                }
+
+                else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    binding.emailREt.setError("Enter valid email!");
+                    binding.emailREt.requestFocus();
+                    return;
+                }
+
+                else if(username.isEmpty()){
+                    binding.usernameREt.setError("Enter Username!");
+                    binding.usernameREt.requestFocus();
+                    return;
+                }
+
+                else if(password.isEmpty()){
+                    binding.passwordREt.setError("Enter Password!");
+                    binding.passwordREt.requestFocus();
+                    return;
+                }
+
+                else if(confirmpassword.isEmpty()){
+                    binding.confirmpasswordREt.setError("Confirm Password!");
+                    binding.confirmpasswordREt.requestFocus();
+                    return;
+                }
+
+                else if(!confirmpassword.equals(password)){
+                    binding.confirmpasswordREt.setError("Password doesn't Match!");
+                    binding.confirmpasswordREt.requestFocus();
+                    return;
+                }
+
+
+            }
+        });
     }
 }
